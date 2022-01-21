876ea17e4e02bf6c406242c1d9b1d549a2869b9b
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-01-21 16:09:53 -0300

    Commit inicial;

---
+++ MDMCFU/MDMCFU/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Problem
+{
+    public static void Main()
+    {
+        string s = "ceabaacb";
+        List<BuffItem> buff = new();
+        buff.Add(new BuffItem(1, s.First()));
+        for (int i = 1; i < s.Length; i++)
+        {
+            bool isInBuff = false;
+            for (int j = 0; j < buff.Count; j++)
+            {
+                if (buff[j].Caractere == s[i])
+                {
+                    isInBuff = true;
+                    buff[j].Quantidade++;
+                }
+            }
+            if (!isInBuff)
+            {
+                buff.Add(new BuffItem(1, s[i]));
+            }
+        }
+        int sumReturn = 0;
+
+        for (int i = 0; i < buff.Count; i++)
+        {
+            for (int j = 0; j < buff.Count; j++)
+            {
+                if (buff[i].Quantidade == buff[j].Quantidade && buff[i].Quantidade != 0 && i != j)
+                {
+                    buff[j].Quantidade--;
+                    sumReturn++;
+                }
+            }
+        }
+        Console.WriteLine(sumReturn);
+    }
+}
+
+public class BuffItem
+{
+    public BuffItem(int i, char s)
+    {
+        Quantidade = i;
+        Caractere = s;
+    }
+    public int Quantidade { get; set; }
+    public char Caractere { get; set; }
+}
